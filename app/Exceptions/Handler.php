5215b4f3fd16cccb3450c4e72629dd697d3de41c
Jadiel dos Santos <jadiel.dossantos@bairesdev.com>
2022-06-22 18:44:48 -0300

    feat: added auth and configured the API so it's now working fine

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,49 +2,61 @@
 
 namespace App\Exceptions;
 
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
-class Handler extends ExceptionHandler
-{
-    /**
-     * A list of exception types with their corresponding custom log levels.
-     *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
-     */
-    protected $levels = [
-        //
-    ];
+class Handler extends ExceptionHandler {
+	/**
+	 * A list of exception types with their corresponding custom log levels.
+	 *
+	 * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
+	 */
+	protected $levels = [
+		//
+	];
 
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array<int, class-string<\Throwable>>
-     */
-    protected $dontReport = [
-        //
-    ];
+	/**
+	 * A list of the exception types that are not reported.
+	 *
+	 * @var array<int, class-string<\Throwable>>
+	 */
+	protected $dontReport = [
+		//
+	];
 
-    /**
-     * A list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
-    protected $dontFlash = [
-        'current_password',
-        'password',
-        'password_confirmation',
-    ];
+	/**
+	 * A list of the inputs that are never flashed to the session on validation exceptions.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $dontFlash = [
+		'current_password',
+		'password',
+		'password_confirmation',
+	];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
-    }
+	/**
+	 * Register the exception handling callbacks for the application.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		$this->reportable(function (Throwable $e) {
+			//
+		});
+	}
+
+	public function render($request, Exception|Throwable $e) {
+		// This will replace our 404 response with
+		// a JSON response.
+		if ($e instanceof ModelNotFoundException && $request->wantsJson()) {
+			return response()->json([
+				'data' => 'Resource not found'
+			], 404);
+		}
+
+		return parent::render($request, $e);
+	}
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,14 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Artisan;
 
-abstract class TestCase extends BaseTestCase
-{
-    use CreatesApplication;
+abstract class TestCase extends BaseTestCase {
+	use CreatesApplication, DatabaseMigrations;
+
+	public function setUp(): void {
+		parent::setUp();
+		Artisan::call('db:seed');
+	}
 }
